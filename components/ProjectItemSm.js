2de411329c5c2ab3f9a88c2ca15739551a5e2ae0
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-02-06 20:47:34 +0100

    Add external links

--- components/ProjectItemSm.js
+++ components/ProjectItemSm.js
@@ -1,36 +1,33 @@
 import Link from "next/link";
 import React from "react";
 
-function ProjectItemSm({ title, projectUrl = null }) {
+function ProjectItemSm({ title, demoUrl, codeUrl, projectUrl = null }) {
   return (
     <div className="relative flex flex-col items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r ">
       <h3 className="text-2xl text-black tracking-wider text-center pb-4 pt-2">
         {title}
       </h3>
-      {projectUrl !== null ? (
-        <div className="flex flex-col items-center">
+      {projectUrl !== null && (
+        <div className="flex flex-col items-center gap-8 p-6">
           <Link href={projectUrl}>
-            <button className="px-8 py-2">More info</button>
-          </Link>
-          <div className="flex items-center justify-center gap-4">
-            <Link href="https://kmk00-user-dashboard.netlify.app/">
-              <button className="px-8 py-2 mt-4 ">Demo</button>
-            </Link>
-            <Link href="https://github.com/kmk00/userdashboard">
-              <button className="px-8 py-2 mt-4">Code</button>
-            </Link>
-          </div>
-        </div>
-      ) : (
-        <div className="">
-          <Link href="https://kmk00-user-dashboard.netlify.app/">
-            <button className="px-8 py-2 mt-4 mr-8">Demo</button>
-          </Link>
-          <Link href="https://github.com/kmk00/userdashboard">
-            <button className="px-8 py-2 mt-4">Code</button>
+            <button className="px-8 py-2 hover:scale-105 duration-200 ease-in-out">
+              More info
+            </button>
           </Link>
         </div>
       )}
+      <div className="flex items-center justify-center gap-16 mb-6">
+        <Link href={demoUrl}>
+          <button className="px-8 py-2 mt-4 hover:scale-105 duration-200 ease-in-out">
+            Demo
+          </button>
+        </Link>
+        <Link href={codeUrl}>
+          <button className="px-8 py-2 mt-4 hover:scale-105 duration-200 ease-in-out">
+            Code
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
--- components/Projects.js
+++ components/Projects.js
@@ -11,16 +11,42 @@ export const Projects = () => {
           <ProjectItemSm
             title="Japanese Practice - Alphabet"
             projectUrl="/japanese"
+            demoUrl="https://kmk00-practice-japanese.netlify.app/"
+            codeUrl="https://github.com/kmk00/practice-japanese"
           />
           <ProjectItemSm
             title="Pokecards - Memory Game"
             projectUrl="/memoryGame"
+            demoUrl="https://pokecard-memory.netlify.app/"
+            codeUrl="https://github.com/kmk00/MemoryCardGame"
           />
-          <ProjectItemSm title="Portfolio Website" projectUrl="/portfolio" />
-          <ProjectItemSm title="User Dashboard Panel" projectUrl="/dashboard" />
-          <ProjectItemSm title="Message Me - Text App" />
-          <ProjectItemSm title="Weather App" />
-          <ProjectItemSm title="Trivia Quizz App" />
+          <ProjectItemSm
+            title="Portfolio Website"
+            projectUrl="/portfolio"
+            demoUrl="/"
+            codeUrl="https://github.com/kmk00/portfolio-website"
+          />
+          <ProjectItemSm
+            title="User Dashboard Panel"
+            projectUrl="/dashboard"
+            demoUrl="https://kmk00-user-dashboard.netlify.app/"
+            codeUrl="https://github.com/kmk00/userdashboard"
+          />
+          <ProjectItemSm
+            title="Message Me - Text App"
+            demoUrl="https://kmk00-message-me.netlify.app/"
+            codeUrl="https://github.com/kmk00/messageMe"
+          />
+          <ProjectItemSm
+            title="Weather App"
+            demoUrl="https://kmk00-weatherapp.netlify.app/"
+            codeUrl="https://github.com/kmk00/WeatherAppv2"
+          />
+          <ProjectItemSm
+            title="Trivia Quizz App"
+            demoUrl="https://randomquizzz.netlify.app/"
+            codeUrl="https://github.com/kmk00/quizzz"
+          />
         </div>
       </div>
     </div>
